a2a0b1bc2f9e853bd1c91e269aa351a5b8a0978c
Quoc Khanh Nguyen <quockhanh.nguyen@pentasys.de>
2020-01-29 14:24:17 +0100

    Test

---
+++ src/test/java/basic/openshift/BasicOpenshiftApplicationTests.java
@@ -0,0 +1,13 @@
+package basic.openshift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicOpenshiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
